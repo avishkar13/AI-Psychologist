4b2b1998f3e4f9796505c5b22e10098dcc1b5aa8
Avishkar Deogharia <avishkardeogharia2004@gmail.com>
2025-10-28 12:56:25 +0530

    Packahejson update

--- client/src/pages/Chatpage.tsx
+++ client/src/pages/Chatpage.tsx
@@ -1,13 +1,10 @@
-import Sidebar from "../components/SidebarLayout";
+import SidebarLayout from "../components/SidebarLayout";
 import ChatWindow from "../components/ChatWindow";
 
 export default function ChatPage() {
   return (
-    <div className="flex h-screen text-gray-900 dark:text-white">
-      <Sidebar />
-      <div className="flex-1">
-        <ChatWindow />
-      </div>
-    </div>
+    <SidebarLayout>
+      <ChatWindow />
+    </SidebarLayout>
   );
 }
